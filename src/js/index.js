cdd4529b6902b0f4935327b0b9f2d6e2bdf2cbe3
Ivan Bokhvalov <bvancha@gmail.com>
2022-10-08 19:17:05 +0200

    saving currentPage to localStorage and add data-atribute(id) for each movie

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,31 @@
 import { getPopularMovies } from "./getPopularMovies";
+import localStrg from "./localStrg";
 export const APIKEY = "565e4989d784811de7dff7d665000157";
 export const APIURL="https://api.themoviedb.org/";
+const refs = {
+    mainContainer: document.querySelector(".filmoteka__container")
+}
 
-
-getPopularMovies(APIKEY);
+renderPopularMovies();
+async function renderPopularMovies(){
+    const currentPageContent = await getPopularMovies(APIKEY);
+    localStrg.save("currentPage",currentPageContent);
+    console.log(currentPageContent);
     
-
+    const markup = currentPageContent.map (({id,title,year,genres,popularity,imgPath}) => {
+        return `<div class="filmoteka__item" data-id="${id}">
+        <a class="filmoteka__item-link" href="./">
+            <div class="filmoteka__item-wrapper">
+                <img class="filmoteka-img" src="${imgPath}" alt="" width="395px">
+                <div class="overlay-text">
+                    <h2 class="subtitle">${title}</h2>
+                    <p class="discription">${genres} | ${year}</p>
+                </div>
+            </div>
+        </a>
+    </div>`
+    })
+    console.log(markup);
+    refs.mainContainer.insertAdjacentHTML("beforeend",markup)
+}
 
