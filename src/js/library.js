4235df3e6622780de5c12ffeefc41630d7b58c74
Maxim <31155487+KozinM@users.noreply.github.com>
2022-10-09 19:18:43 +0300

    library render major update (#18)
    created and added to project files:
     - library.js ( a) rendering films' library gallery on opening of library.html;
    b) added function for dummy library variable creation - for testing purposes)
    - main-library.html (created partial for filmoteka__container in library.html)
    
    modified:
    - libraryRender.js (minor changes: comments correction, small code modifications for proper function working)
    - library.html (added library.js src and main-library.html src)

---
+++ src/js/library.js
@@ -0,0 +1,23 @@
+import { getPopularMovies } from "./getPopularMovies";
+import localStrg from "./localStrg";
+import { libraryRender } from "./libraryRender";
+export const APIKEY = "565e4989d784811de7dff7d665000157";
+export const APIURL="https://api.themoviedb.org/";
+const refs = {
+    mainContainer: document.querySelector(".filmoteka__container")
+}
+
+/* for testing purpouses getting film collection and save it as
+it is library variable */
+
+
+dummyLibraryMovies();
+async function dummyLibraryMovies(){
+    testQuery = await getPopularMovies(APIKEY);
+    localStrg.save("watched",testQuery);
+    localStrg.save("queued", testQuery);
+}
+//console.log("I'm before library render")
+
+/*reating gallery with films from "watched" library*/
+libraryRender(refs.mainContainer,"watched");
--- src/js/libraryRender.js
+++ src/js/libraryRender.js
@@ -1,48 +1,61 @@
 //importing library for working with local storage
-import { load, save, del } from './localStrg';
-f;
+import localStrg from './localStrg';
 
 /*function definition for gallery rendering
 with two parameters: 1) document element where gallery has to be created
 2) library (local storage variable) from which this gallery should be created*/
-function libraryRender(htmlElement, userLibrary) {
-  //getting films collection from localStorage
-  const filmCollectionFromLocalStorage = load(userLibrary);
 
-  //Checking if there is collection variable in local storage
+function libraryRender(htmlElement, userLibrary) {
+
+  
+  //console.log(htmlElement);
+  
+  
+  /* getting films collection from localStorage */
+
+  const filmCollectionFromLocalStorage = localStrg.load(userLibrary);
+  
+  //console.log("read from localstorage"+filmCollectionFromLocalStorage);
+  
+  /* Checking if there is collection variable in local storage */
+
   if (!filmCollectionFromLocalStorage) {
-    //if not: adding title with text "It seems that there is no films here!"
+
+    /*if not: adding title with text "It seems that there is no films here!"*/
+
     htmlElement.insertAdjacentHTML(
       'beforeend',
       '<div><p2 class="empty_text">It seems that there is no films here!</p2></div>'
     );
   }
-  //if collection exists, creating gallery
+
+  /* if collection exists, creating gallery */
+
   else {
-    const ObjectForInsert = filmCollectionFromLocalStorage
-      .map(film => {
+    //console.log("local storage variable was read successfully!");
+    let ObjectForInsert = filmCollectionFromLocalStorage.map(
+      film => {
+        //console.log("film: " + film);
         const {
+          id,
           title,
-          originalTitle,
           year,
           genres,
-          /* popularity, 
-                overview, */
           rating,
-          /* voteCount, */
           imgPath,
         } = film;
-        let itemString = `<div class="filmoteka__item">
+
+        let itemString = `<div class="filmoteka__item" data-id="${id}">
         <a class="filmoteka__item-link" href="./">
             <div class="filmoteka__item-wrapper">
                 <img class="filmoteka-img" src="${imgPath}" alt="" width="">
                 <div class="overlay-text">
-                    <h2 class="subtitle">${originalTitle}</h2>
+                    <h2 class="subtitle">${title}</h2>
                     <p class="discription">`;
-        if (!genres === undefined) {
+        if (!(genres === undefined)) {
           itemString += `<span class="description__genre">${genres}</span>`;
         }
-        if (!year === undefined) {
+        if (!(year === undefined)) {
           itemString += `<span class="description__year">${year}</span>`;
         }
         itemString += `<span class="rating">${rating}</span></p>
@@ -50,32 +63,13 @@ function libraryRender(htmlElement, userLibrary) {
             </div>
         </a>
         </div>`;
-        /* if (genres === undefined) {
-          return `<div class="filmoteka__item">
-            <a class="filmoteka__item-link" href="./">
-                <div class="filmoteka__item-wrapper">
-                    <img class="filmoteka-img" src="${imgPath}" alt="" width="">
-                    <div class="overlay-text">
-                        <h2 class="subtitle">${originalTitle}</h2>
-                        <p class="discription">${genres} | ${year} <span class="rating">${rating}</span></p>
-                    </div>
-                </div>
-            </a>
-        </div>`;
-        } else {
-          return `<div class="filmoteka__item">
-            <a class="filmoteka__item-link" href="./">
-                <div class="filmoteka__item-wrapper">
-                    <img class="filmoteka-img" src="${imgPath}" alt="" width="">
-                    <div class="overlay-text">
-                        <h2 class="subtitle">${originalTitle}</h2>
-                        <p class="discription">${year} <span class="rating">${rating}</span></p>
-                    </div>
-                </div>
-            </a>
-        </div>`;*/
+        return itemString;
       })
       .join('');
+      
+      //console.log( "this is object for insert: "+ObjectForInsert+ "In here");
+      //console.log("I've done with string gathering");
+      
 
     htmlElement.insertAdjacentHTML('beforeend', ObjectForInsert);
   }
