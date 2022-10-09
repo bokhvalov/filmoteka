545dd928702174160da25143204acef5fe2c8a4a
Maxim <31155487+KozinM@users.noreply.github.com>
2022-10-09 12:20:53 +0300

    Update libraryRender.js
    minor changes in function logic + added comments

--- src/js/libraryRender.js
+++ src/js/libraryRender.js
@@ -6,22 +6,19 @@ f;
 with two parameters: 1) document element where gallery has to be created
 2) library (local storage variable) from which this gallery should be created*/
 function libraryRender(htmlElement, userLibrary) {
-
-//getting films collection from localStorage
+  //getting films collection from localStorage
   const filmCollectionFromLocalStorage = load(userLibrary);
 
-//Checking if there is collection variable in local storage
-  if (!filmCollectionFromLocalStorage) 
-  
-//if not: adding title with text "It seems that there is no films here!"
-  {
+  //Checking if there is collection variable in local storage
+  if (!filmCollectionFromLocalStorage) {
+    //if not: adding title with text "It seems that there is no films here!"
     htmlElement.insertAdjacentHTML(
       'beforeend',
       '<div><p2 class="empty_text">It seems that there is no films here!</p2></div>'
     );
-  } else 
+  }
   //if collection exists, creating gallery
-  {
+  else {
     const ObjectForInsert = filmCollectionFromLocalStorage
       .map(film => {
         const {
@@ -35,7 +32,25 @@ function libraryRender(htmlElement, userLibrary) {
           /* voteCount, */
           imgPath,
         } = film;
-        if (genres === undefined) {
+        let itemString = `<div class="filmoteka__item">
+        <a class="filmoteka__item-link" href="./">
+            <div class="filmoteka__item-wrapper">
+                <img class="filmoteka-img" src="${imgPath}" alt="" width="">
+                <div class="overlay-text">
+                    <h2 class="subtitle">${originalTitle}</h2>
+                    <p class="discription">`;
+        if (!genres === undefined) {
+          itemString += `<span class="description__genre">${genres}</span>`;
+        }
+        if (!year === undefined) {
+          itemString += `<span class="description__year">${year}</span>`;
+        }
+        itemString += `<span class="rating">${rating}</span></p>
+                </div>
+            </div>
+        </a>
+        </div>`;
+        /* if (genres === undefined) {
           return `<div class="filmoteka__item">
             <a class="filmoteka__item-link" href="./">
                 <div class="filmoteka__item-wrapper">
@@ -58,8 +73,7 @@ function libraryRender(htmlElement, userLibrary) {
                     </div>
                 </div>
             </a>
-        </div>`;
-        }
+        </div>`;*/
       })
       .join('');
 
