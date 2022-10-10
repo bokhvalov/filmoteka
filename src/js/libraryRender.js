641603fde67ade062d2e305629b0432947ede7fe
Maxim <31155487+KozinM@users.noreply.github.com>
2022-10-10 19:29:06 +0300

    major update to libraryRender.js, minor fix to header-library
    1) library.js:
    - added event listiners to buttons "watched" and "queued"
    2) header-library.html:
    - added "id" to buttons
    3)libraryRender.js:
    - added new functions: libraryCleaner, isInLibrary, getFilmById, addFilm, removeFilmById

--- src/js/libraryRender.js
+++ src/js/libraryRender.js
@@ -74,4 +74,51 @@ function libraryRender(htmlElement, userLibrary) {
     htmlElement.insertAdjacentHTML('beforeend', ObjectForInsert);
   }
 }
-export { libraryRender };
+/*Clear all nested elements in html element*/
+function libraryCleaner(htmlElement){
+  htmlElement.innerHTML = '';
+}
+
+/*boolean function to check if a film is in a library*/
+function isInLibrary(filmId, userLibrary) {
+  
+  const filmCollectionFromLocalStorage = localStrg.load(userLibrary);
+
+  if (!filmCollectionFromLocalStorage) {
+    return false;
+  }
+  filmCollectionFromLocalStorage.forEach((film)=>{
+    const {id}=film; if(id===filmId) {return true;}});
+  return false;
+}
+
+/*return object film from specified library, if there is no film with given id - 
+function returns NULL*/
+
+function getFilmById(filmId, userLibrary) {
+  const filmLibrary = localStrg.load(userLibrary);
+  if (!filmLibrary) {
+     return null;}
+  return filmLibrary.find((film) => film.id === filmId);;
+}
+
+/*function adds film object to specified library*/
+function addFilm(film, userLibrary) {
+  let filmLibrary = localStrg.load(userLibrary);
+  if (!filmLibrary) {
+    localStrg.save(userLibrary, [film]);
+  }
+  filmLibrary.push(film);
+  localStrg.save(userLibrary, filmLibrary);
+}
+
+/*function removes film with specified id from specified library*/
+function removeFilmById(filmId, userLibrary){
+  if (isInLibrary(filmId, userLibrary)) {
+    let filmLibrary = localStrg.load(userLibrary);
+    filmLibrary = filmLibrary.filter((film) => film.id !== filmId);
+    localStrg.save(userLibrary.filmLibrary);
+  }
+}
+
+export { libraryRender, libraryCleaner, getFilmById, addFilm, removeFilmById, isInLibrary};
