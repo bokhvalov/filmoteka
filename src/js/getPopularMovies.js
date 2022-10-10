787c6c89747b1838536cf8f34e62638c5cac95ba
Ivan Bokhvalov <103508644+bokhvalov@users.noreply.github.com>
2022-10-10 09:18:14 +0200

    Dev (#23)
    * Fetch popular function + function for working with local storage + decoder for genres (#3)
    
    * fetchPopular
    
    * detchPopular function implementation
    
    Added :
    - fetchPopular,
    - script for working with local storage, - script for decoding genres
    
    * Fetch popular (#6)
    
    * fetchPopular
    
    * detchPopular function implementation
    
    Added :
    - fetchPopular,
    - script for working with local storage, - script for decoding genres
    
    * Fetch popular function + function for working with local storage +
    
    * Fetch popular (#7)
    
    * fetchPopular
    
    * detchPopular function implementation
    
    Added :
    - fetchPopular,
    - script for working with local storage, - script for decoding genres
    
    * Fetch popular function + function for working with local storage +
    
    * Update deploy.yml
    
    * header (#5)
    
    Co-authored-by: Ivan Bokhvalov <103508644+bokhvalov@users.noreply.github.com>
    
    * Feature/footer html (#10)
    
    * draft
    
    * added css
    
    Co-authored-by: Екатерина <Katderl@ukr.net>
    
    * Feature/main (#11)
    
    * create main
    
    * Update _main.scss
    
    * Add images
    
    Co-authored-by: InnaShtondenko <inna.mycase@gmail.com>
    
    * saving currentPage to localStorage and add data-atribute(id) for each movie
    
    * Feache modal (#14)
    
    * 1
    
    * 22
    
    * fix "," before items in main
    
    * visually-hidden (#15)
    
    * fix headers (#16)
    
    * Feature js library render (#17)
    
    * 21 (#19)
    
    реалізував таблицю і трохи поправив стил
    
    * library render major update (#18)
    
    created and added to project files:
     - library.js ( a) rendering films' library gallery on opening of library.html;
    b) added function for dummy library variable creation - for testing purposes)
    - main-library.html (created partial for filmoteka__container in library.html)
    
    modified:
    - libraryRender.js (minor changes: comments correction, small code modifications for proper function working)
    - library.html (added library.js src and main-library.html src)
    
    * Feature js library render (#21)
    
    * library render major update
    
    created and added to project files:
     - library.js ( a) rendering films' library gallery on opening of library.html;
    b) added function for dummy library variable creation - for testing purposes)
    - main-library.html (created partial for filmoteka__container in library.html)
    
    modified:
    - libraryRender.js (minor changes: comments correction, small code modifications for proper function working)
    - library.html (added library.js src and main-library.html src)
    
    * Update library.js
    
    changes after meeting:
     - commented import of getPopularMovies
     - commented dummyLibraryMovies for creating testing variables
    
    Co-authored-by: Ivan Bokhvalov <103508644+bokhvalov@users.noreply.github.com>
    
    * Update deploy.yml
    
    Co-authored-by: Ihor <102248912+IhorMal@users.noreply.github.com>
    Co-authored-by: Екатерина <Katderl@ukr.net>
    Co-authored-by: InnaShtondenko <inna.mycase@gmail.com>
    Co-authored-by: yanchenko04 <95707293+yanchenko04@users.noreply.github.com>
    Co-authored-by: Maxim <31155487+KozinM@users.noreply.github.com>

--- src/js/getPopularMovies.js
+++ src/js/getPopularMovies.js
@@ -1,5 +1,6 @@
 import { fetchPopular } from './API';
 import { decodeGenres } from './decodeGenres';
+
 import { APIKEY } from './index';
 import { APIURL } from './index';
 
@@ -8,40 +9,49 @@ export async function getPopularMovies(APIKEY) {
   const pageCount = popularMovies.total_pages;
   const result = popularMovies.results;
 
-  const currentPage = result.map(
-    async ({
-      title,
-      original_title,
-      name,
-      original_name,
-      overview,
-      poster_path,
-      genre_ids,
-      popularity,
-      release_date,
-      first_air_date,
-      vote_average,
-      vote_count,
-    }) => {
-      const movieTitle = title ? title : name;
-      const movieOriginalTitle = original_title
-        ? original_title
-        : original_name;
-      const movieDate = release_date ? release_date : first_air_date;
+  const currentPage = await Promise.all(
+    result.map(
+      async ({
+        id,
+        title,
+        original_title,
+        name,
+        original_name,
+        overview,
+        poster_path,
+        genre_ids,
+        popularity,
+        release_date,
+        first_air_date,
+        vote_average,
+        vote_count,
+      }) => {
+        const movieTitle = title ? title : name;
+        const movieOriginalTitle = original_title
+          ? original_title
+          : original_name;
+        const movieDate = release_date ? release_date : first_air_date;
+        const imgPath = poster_path
+          ? `https://image.tmdb.org/t/p/w300` + poster_path
+          : '';
+        const genres = genre_ids ? await decodeGenres(genre_ids) : null;
+        const raitng = vote_average ? vote_average : null;
 
-      const movieData = {
-        title: movieTitle,
-        originalTitle: movieOriginalTitle,
-        year: movieDate.slice(0, 4),
-        genres: await decodeGenres(genre_ids),
-        popularity: popularity,
-        overview: overview,
-        rating: vote_average,
-        voteCount: vote_count,
-        imgPath: APIURL + 't/p/orginal' + poster_path,
-      };
-      console.log(movieData);
-      return movieData;
-    }
+        return await {
+          id: id,
+          title: movieTitle,
+          originalTitle: movieOriginalTitle,
+          year: movieDate.slice(0, 4),
+          genres: genres,
+          popularity: popularity,
+          overview: overview,
+          rating: raitng,
+          voteCount: vote_count,
+          imgPath: imgPath,
+        };
+      }
+    )
   );
+console.log(currentPage);
+  return currentPage;
 }
