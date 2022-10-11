20031dae88cb9acb01e0e0cce46545893bf9da94
Artom Makhun <89594726+MakhunArtom@users.noreply.github.com>
2022-10-11 10:47:02 +0300

    featch/pag (#33)

--- src/js/API.js
+++ src/js/API.js
@@ -1,13 +1,18 @@
-import { APIURL } from "./index";
-import localStrg from "./localStrg";
+import { APIURL } from './index';
+import localStrg from './localStrg';
+import { PAGE } from './pagination-js/main-pagination';
 
-export async function fetchPopular(token){
-    return await (await fetch(APIURL+`3/trending/movie/day?api_key=${token}`)).json();
+export async function fetchPopular(token) {
+  return await (
+    await fetch(APIURL + `3/trending/movie/day?api_key=${token}&page=${PAGE}`)
+  ).json();
 }
 
-export async function fetchGenresList(token){
-    const response = await (await fetch(APIURL+`3/genre/movie/list?api_key=${token}&language=en-US`)).json();
-    localStrg.save("genresList",response.genres);
-    console.log("Genres were updated, and saved to localStorage for future");
-    return response.genres;
+export async function fetchGenresList(token) {
+  const response = await (
+    await fetch(APIURL + `3/genre/movie/list?api_key=${token}&language=en-US`)
+  ).json();
+  localStrg.save('genresList', response.genres);
+  console.log('Genres were updated, and saved to localStorage for future');
+  return response.genres;
 }
