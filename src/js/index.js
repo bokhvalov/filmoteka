177c031892bf610eaeac22fcc0ef3bb7838a2d3f
Ivan Bokhvalov <bvancha@gmail.com>
2022-10-11 15:10:56 +0200

    adding slice for genres on gallery and spaces on genres generationand

--- src/js/index.js
+++ src/js/index.js
@@ -23,12 +23,19 @@ export async function renderPopularMovies() {
 
   const markup = currentPageContent.map(
     ({ id, title, year, genres, rating, imgPath }) => {
+      let countOfComma = (genres.match(/\,/g) || []).length;
+      while (countOfComma>1) {
+        genres = genres.slice(0, genres.lastIndexOf(","))+"...";
+        countOfComma = (genres.match(/\,/g) || []).length;
+      }
+
+
       return `<div class="filmoteka__item" data-id="${id}">
             <div class="filmoteka__item-wrapper">
                 <img class="filmoteka-img" src="${imgPath}" alt="">
                 <div class="overlay-text">
                     <h2 class="subtitle">${title}</h2>
-                    <p class="discription">${genres}<span class="filmoteka__year"> ${year}</span> </p>
+                    <p class="discription">${genres} <span class="filmoteka__year"> ${year}</span> </p>
                     <div class="filmoteka__rating">${rating}</div>
                 </div>
             </div>
