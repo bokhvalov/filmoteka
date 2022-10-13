55f3b9df722bc7ba83ada9b5497d477f79d2e114
SlavaOSO <98713732+SlavaOSO@users.noreply.github.com>
2022-10-13 14:39:38 +0300

    feature/spinner

--- src/js/index-page/search.js
+++ src/js/index-page/search.js
@@ -6,6 +6,9 @@ import { startPage } from '../pagination-js/counter-pagination';
 import { renderItems } from '../common/renderItems';
 import { renderPopularMovies } from '.';
 import localStrg from '../localStorage/localStrg';
+import Spinner from '../common/spinner';
+
+const spin = new Spinner();
 
 export let pageCount;
 
@@ -13,11 +16,13 @@ let searchQuery = '';
 
 export async function searchMovies(event) {
   event.preventDefault();
+  spin.spinOn();
   searchQuery = event.target.search.value;
   refs.searchResultText.classList.add('visually-hidden');
 
   if(!searchQuery){
     renderPopularMovies();
+    spin.spinOff();
     return
   }
 
@@ -27,6 +32,7 @@ export async function searchMovies(event) {
   if (currentPageContent.length > 0) {
     renderItems(currentPageContent);
     startPage();
+    spin.spinOff();
     return;
   }
   refs.searchResultText.classList.remove('visually-hidden');
