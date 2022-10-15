248083a1112c900211b4a2c56b46af5619ebe014
InnaShtondenko <inna.mycase@gmail.com>
2022-10-15 22:24:46 +0300

    Update themoviedbAPI.js

--- src/js/index-page/themoviedbAPI.js
+++ src/js/index-page/themoviedbAPI.js
@@ -2,20 +2,34 @@ import { APIURL } from './index';
 import localStrg from '../localStorage/localStrg';
 import { PAGE } from '../pagination-js/main-pagination';
 
+
 export async function fetchPopular(token) {
+  let pageLang = localStorage.getItem('lang');
+    if (pageLang === 'ua') {
+      lang = `uk`;
+    } else {
+      lang = `en`;
+    }
   return await (
-    await fetch(APIURL + `3/trending/movie/day?api_key=${token}&page=${PAGE}`)
+    await fetch(APIURL + `3/trending/movie/day?api_key=${token}&language=${lang}&page=${PAGE}`)
   ).json();
 }
 
-export async function fetchGenresList(token){
-    const response = await (await fetch(APIURL+`3/genre/movie/list?api_key=${token}&language=en-US`)).json();
+export async function fetchGenresList(token, lang) {
+
+    const response = await (await fetch(APIURL+`3/genre/movie/list?api_key=${token}&language=${lang}`)).json();
     localStrg.save("genresList",response.genres);
     console.log("Genres were updated, and saved to localStorage for future");
     return response.genres;
 }
 
-export async function fetchSearch(token,name) {   
-    const response = await (await fetch(APIURL+`3/search/movie?api_key=${token}&language=en-US&query=${name}`)).json()
+export async function fetchSearch(token, name, lang) {  
+  let pageLang = localStorage.getItem('lang');
+    if (pageLang === 'en') {
+      lang = `en`;
+    } else {
+      lang = `uk`;
+    }
+    const response = await (await fetch(APIURL+`3/search/movie?api_key=${token}&language=e${lang}&query=${name}`)).json()
     return response
 }
