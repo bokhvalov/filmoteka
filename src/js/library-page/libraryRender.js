30b9b75b0ff74318d94ff36ba44cb3a75e494f6e
InnaShtondenko <inna.mycase@gmail.com>
2022-10-15 22:24:59 +0300

    Update libraryRender.js

--- src/js/library-page/libraryRender.js
+++ src/js/library-page/libraryRender.js
@@ -1,9 +1,10 @@
 import localStrg from '../localStorage/localStrg';
 import { renderItems } from '../common/renderItems';
 
+
 export function libraryRender(userLibrary) {
     const mainContainer = document.querySelector('.filmoteka__container');
-    let currentLib = localStrg.load(userLibrary);
+  let currentLib = localStrg.load(userLibrary);
       if (!currentLib) {
         mainContainer.innerHTML =
           '<div><p2 class="empty_text">It seems that there is no films here!</p2></div>'
