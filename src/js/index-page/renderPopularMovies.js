69451ed27f2c0b5a9bd86da27171c8abaf1906fe
MakhunArtom <super_makhun@ukr.net>
2022-10-16 17:15:27 +0300

    featch-caunnter

--- src/js/index-page/renderPopularMovies.js
+++ src/js/index-page/renderPopularMovies.js
@@ -1,15 +1,18 @@
+import { startPage } from '../pagination-js/counter-pagination';
 import { processCurrentPage } from '../data-processing/processCurrentPage';
 import { getPopularMovies } from '../index-page/search';
 import { renderItems } from '../common/renderItems';
-import { APIKEY } from "../index-page/index";
+import { APIKEY } from '../index-page/index';
 import Spinner from '../common/spinner';
 
 const spin = new Spinner();
 
 export async function renderPopularMovies() {
-    spin.spinOn();
-    const popularMovies = await getPopularMovies(APIKEY);
-    const currentPageContent = await processCurrentPage(popularMovies);
-    spin.spinOff();
-    renderItems(currentPageContent);
-  }
+  spin.spinOn();
+  const popularMovies = await getPopularMovies(APIKEY);
+  const currentPageContent = await processCurrentPage(popularMovies);
+  spin.spinOff();
+  renderItems(currentPageContent);
+
+  startPage();
+}
